e1c4d9794ad8cb4ced97c28e441fa8fa0783afd7
arfinaleusuf <arfinaleusuf@gmail.com>
2026-04-27 00:30:47 +0600

    inharitance

--- inheritance.py
+++ inheritance.py
@@ -1,34 +1,33 @@
-class Leptop:
-    def __init__(self, brand, price, color, memory) -> None:
+#  base class , parent class, commom attribute + functional class
+#  derived class, child class, uncommon attribite + functionality class
+
+class Gadget:
+    def __init__(self, brand, price, color, origin):
         self.brand = brand
         self.price = price
         self.color = color
-        self.memory = memory
-
+        self.origin = origin
     def run(self):
         return f'running laptop {self.brand}'
-    
+
+class Leptop:
+    def __init__(self, memory, ssd) -> None:
+        self.memory = memory
+        self.ssd = ssd
+  
     def coding(self):
         return f'learning python and practicing'
     
 class Phone:
-    def __init__(self, brand, price, color, dual_sim):
-        self.brand = brand
-        self.price = price
-        self.color = color
+    def __init__(self, dual_sim):
+ 
         self.dual_sim = dual_sim
     def phone_call(self, number, text):
         return f'Sending SMS to: {number} with {text}'
-    def run(self):
-        return f'phone tipa tipi kore'
+
 class Camera:
-    def __init__(self, brand,price, color, pixel):
-        self.brand = brand
-        self.price = price
-        self.color = color
+    def __init__(self,pixel):
         self.pixel = pixel
-    def run(self):
-        pass
     def change_lens(self):
         pass
     
