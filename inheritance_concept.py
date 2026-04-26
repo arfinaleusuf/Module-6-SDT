8b1f59fc6c76d4678f3f48224f1de44843c180cc
arfinaleusuf <arfinaleusuf@gmail.com>
2026-04-27 02:13:15 +0600

    add multiple inheritance example with Family, School, and Sports classes

--- inheritance_concept.py
+++ inheritance_concept.py
@@ -11,4 +11,9 @@ class DeriveClass(BaseClass):
 1. simple inheritance: parent class --> child class (Gadget ---> Phone) (Gadget ---> Laptop)
 
 2. Multi-leve inheritance: Grandpa --> Parent --> Child (Vehicle --> Bus --> Ac Bus)
+(Vehicle --> Truck --> PickUpTruck)
+
+3. multiple inheritance: Student(Family, School, Sports)
+
+4. Hybrid: Grand --> Father, Uncle, Aunty --> Child (Father, Uncle)
 """
