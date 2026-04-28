4b776a34fd7c54f9ce7b8f8b07ed0e22688bc1ce
arfinaleusuf <arfinaleusuf@gmail.com>
2026-04-28 23:02:12 +0600

    abstruction

--- abstract.py
+++ abstract.py
@@ -1,13 +1,20 @@
-class Animal:
+from abc import ABC, abstractmethod
+#  abstract base class
+
+class Animal(ABC):
+    @abstractmethod  #enforce all derived class to have a eat mathord
     def eat(self):
-        print('eating banana')
+        print(' I name food')
     def move(self):
         pass
 
 class Monkey(Animal):
-    def __init__(self):
-        self.name = 'Monkey'
+    def __init__(self, name):
+        self.catagory = 'Monkey'
+        self.name = name
         super().__init__()
+    def eat(self):
+        print('Hey na nana')
 
 
 layka = Monkey('lucky')
